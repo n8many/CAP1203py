86315fc8a9d2eb259c35a17c9629ec9cc7fdfe33
n8many <n8nyoung@gmail.com>
2020-04-25 19:05:59 -0600

    Add type hints to library

--- cap1203.py
+++ cap1203.py
@@ -1,4 +1,4 @@
-import smbus2 as smbus
+#import smbus2 as smbus
 from enum import IntEnum, IntFlag
 from typing import Union
 
@@ -89,11 +89,16 @@ def get_bits(register: int, index: int, length: int = 1) -> Union[int, bool]:
 
 class CAP1203(object):
 
-    def __init__(self, bus: smbus.SMBus, address: int=ADDRESS):
-        '''
-        Initialize CAP1023 Object with i2c bus and address
-        TODO: Add documentation on inputs
-        '''
+    def __init__(self, bus, address=ADDRESS):
+        """
+        Object for connecting to CAP1023 sensor via i2c.
+
+        :param bus: i2c bus to connect to CAP1203 sensor. Must be compatible with smbus.SMBus object
+        :type bus: smbus.SMBus
+        :param address: i2c address of CAP1203 sensor
+        :type address: int
+        """
+
         # Only one valid address for this board, but in case that changes
         if address != ADDRESS:
             raise ValueError("Invalid Address: {0:#x}".format(address))
@@ -109,6 +114,12 @@ class CAP1203(object):
             raise RuntimeError("Cannot connect to CAP1203")
 
     def is_connected(self):
+        """
+        Check communication with sensor
+
+        :return: Whether sensor is connected
+        :rtype: bool
+        """
         for i in range(5):
             try:
                 self.bus.read_byte(self.address)  # This will raise OSError if device isn't present
@@ -119,23 +130,68 @@ class CAP1203(object):
         return False
 
     def check_main_control(self):
-        reg = self.bus.read_i2c_block_data(self.address, MAIN_CONTROL, 1)[0]
+        """
+        Check main control register
+        """
+        reg = self.read_register(MAIN_CONTROL)
         return
 
     def check_status(self):
-        return
+        """
+        Check CAP1203 for errors
 
-    def set_sensitivity(self, sensitivity: Sensitivity):
-        reg = self.bus.read_i2c_block_data(self.address, SENSITIVITY_CONTROL, 1)[0]
+        :return: Pads with error
+        :rtype: Pad
+        """
+        reg = self.read_register(GENERAL_STATUS)
+        reg_inp = self.read_register(SENSOR_INPUT_STATUS)
+        reg_bc = self.read_register(BASE_COUNT_OUT)
+        bc_pad = Pad(0)
+        error_pad = Pad(0)
+
+        if get_bits(reg, 6):
+            # Base count out of range for a sensor
+            bc_pad = Pad(get_bits(reg_bc, 0, 3))
+            if bc_pad:
+                print(f"Base count out of range for pad(s): {bc_pad}")
+
+        if get_bits(reg, 5):
+            # Calibration failed for a sensor
+            error_pad = Pad(get_bits(reg_inp, 0, 3))
+            if error_pad:
+                print(f"Failed to calibrate pad(s): {error_pad}")
+
+        return bc_pad + error_pad
+
+    def set_sensitivity(self, sensitivity):
+        """
+        Set sensitivity of CAP1203
+
+        :param sensitivity: Value to set sensor sensitivity to
+        :type sensitivity: Sensitivity
+        """
+        reg = self.read_register(SENSITIVITY_CONTROL)
         reg = set_bits(reg, sensitivity, 4, 3)
-        self.bus.write_i2c_block_data(self.address, SENSITIVITY_CONTROL, [reg])
+        self.write_register(SENSITIVITY_CONTROL, reg)
         return
 
-    def get_sensitivity(self) -> Sensitivity:
-        return Sensitivity(get_bits(self.bus.read_i2c_block_data(self.address, SENSITIVITY_CONTROL, 1)[0], 4, 3))
+    def get_sensitivity(self):
+        """
+        Get sensitivity from CAP1203
 
-    def set_interrupt_setting(self, enable: Union[bool, Pad]):
-        reg = self.bus.read_i2c_block_data(self.address, INTERRUPT_ENABLE, 1)[0]
+        :return: Sensitivity setting
+        :rtype: Sensitivity
+        """
+        return Sensitivity(get_bits(self.read_register(SENSITIVITY_CONTROL), 4, 3))
+
+    def set_interrupt_setting(self, enable):
+        """
+        Set interrupt setting of CAP1203
+
+        :param enable: Enable interrupt for some or all pads
+        :type enable: Union[bool, Pad]
+        """
+        reg = self.read_register(INTERRUPT_ENABLE)
         if isinstance(enable, Pad):
             # Enable selected pads
             reg = set_bits(reg, enable, 0, 3)
@@ -143,35 +199,59 @@ class CAP1203(object):
             reg = set_bits(reg, enable, 0)  # Left
             reg = set_bits(reg, enable, 1)  # Middle
             reg = set_bits(reg, enable, 2)  # Right
-        self.bus.write_i2c_block_data(self.address, INTERRUPT_ENABLE, [reg])
+        self.write_register(INTERRUPT_ENABLE, reg)
         return
 
-    def get_interrupt_setting(self) -> Pad:
-        reg = self.bus.read_i2c_block_data(self.address, INTERRUPT_ENABLE, 1)[0]
+    def get_interrupt_setting(self):
+        """
+        Get interrupt setting from CAP1203
+
+        :return: Pads with interrupts enabled
+        :rtype: Pad
+        """
+        reg = self.read_register(INTERRUPT_ENABLE)
         return Pad(get_bits(reg, 0, 3))
 
     def clear_interrupt(self):
-        reg = self.bus.read_i2c_block_data(self.address, MAIN_CONTROL, 1)[0]
+        """
+        Clear interrupt from CAP1203
+        """
+        reg = self.read_register(MAIN_CONTROL)
         reg = set_bits(reg, False, 0)  # Clear flag
-        self.bus.write_i2c_block_data(self.address, MAIN_CONTROL, [reg])
+        self.write_register(MAIN_CONTROL, reg)
         return
 
-    def check_touched(self) -> Pad:
-        # Check touch register without clearing interrupt
-        reg = self.bus.read_i2c_block_data(self.address, SENSOR_INPUT_STATUS, 1)[0]
+    def check_touched(self):
+        """
+        Check for touched pads without clearing interrupt
+
+        :return: Touched pad(s) if any
+        :rtype: Pad
+        """
+        reg = self.read_register(SENSOR_INPUT_STATUS)
         touch_status = Pad(get_bits(reg, 0, 3))
         return touch_status
 
     def get_touched(self):
-        # Get touch register and clear interrupt
+        """
+        Get touch register and clear interrupt
+
+        :return: Touched pad(s) if any
+        :rtype: Pad
+        """
         res = self.check_touched()
         if res is not Pad(0):
             self.clear_interrupt()
         return res
 
     def is_touched(self):
-        # Get whether or not a touch is active, and clear interrupt
-        reg = self.bus.read_i2c_block_data(self.address, GENERAL_STATUS, 1)[0]
+        """
+        Get whether or not a touch is active, and clear interrupt
+
+        :return: Touch status
+        :rtype: bool
+        """
+        reg = self.read_register(GENERAL_STATUS)
         res = get_bits(reg, 0)
         if res:
             self.clear_interrupt()
@@ -179,7 +259,12 @@ class CAP1203(object):
         return False
 
     def is_left_touched(self):
-        # Check left touch bit, and clear interrupt if touched
+        """
+        Check left touch status, and clear interrupt if touched
+
+        :return: Left touch status
+        :rtype: bool
+        """
         res = self.check_touched()
         if Pad.Left in res:
             self.clear_interrupt()
@@ -187,7 +272,12 @@ class CAP1203(object):
         return False
 
     def is_right_touched(self):
-        # Check right touch bit, and clear interrupt if touched
+        """
+        Check right touch status, and clear interrupt if touched
+
+        :return: Right touch status
+        :rtype: bool
+        """
         res = self.check_touched()
         if Pad.Right in res:
             self.clear_interrupt()
@@ -195,7 +285,12 @@ class CAP1203(object):
         return False
 
     def is_middle_touched(self):
-        # Check middle touch bit, and clear interrupt if touched
+        """
+        Check middle touch status, and clear interrupt if touched
+
+        :return: Middle touch status
+        :rtype: bool
+        """
         res = self.check_touched()
         if Pad.Middle in res:
             self.clear_interrupt()
@@ -210,42 +305,115 @@ class CAP1203(object):
 
         return False
 
-    def set_power_button_pad(self, pad: Pad):
-        reg = self.bus.read_i2c_block_data(self.address, POWER_BUTTON, 1)[0]
+    def set_power_button_pad(self, pad):
+        """
+        Set the Pad(s) for which the power button function is enabled.
+        See page 16 for more information on the power button function.
+
+        :param pad: Pad(s) to enable the power button on
+        :type pad: Pad
+        """
+        reg = self.read_register(POWER_BUTTON)
         set_bits(reg, pad, 0, 3)
-        self.bus.write_i2c_block_data(self.address, POWER_BUTTON, [reg])
+        self.write_register(POWER_BUTTON, reg)
         return
 
-    def get_power_button_pad(self) -> Pad:
-        reg = self.bus.read_i2c_block_data(self.address, POWER_BUTTON, 1)[0]
+    def get_power_button_pad(self):
+        """
+        Get the Pad(s) for which the power button function is enabled.
+        See page 16 for more information on the power button function.
+
+        :return: Pad(s) to enable the power button on
+        :rtype: Pad
+        """
+        reg = self.read_register(POWER_BUTTON)
         res = Pad(get_bits(reg, 0, 3))
         return res
 
     def set_power_button_time(self, time: PowerTime):
-        reg = self.bus.read_i2c_block_data(self.address, POWER_BUTTON_CONFIG, 1)[0]
+        """
+        Set the time setting for the power button function.
+        See page 16 for more information on the power button function.
+
+        :param time: Time setting for power button function
+        :type time: PowerTime
+        """
+        reg = self.read_register(POWER_BUTTON_CONFIG)
         set_bits(reg, time, 0, 2)
         return
 
     def get_power_button_time(self) -> PowerTime:
-        reg = self.bus.read_i2c_block_data(self.address, POWER_BUTTON_CONFIG, 1)[0]
+        """
+        Get the time setting for the power button function.
+        See page 16 for more information on the power button function.
+
+        :return: Time setting for power button function
+        :rtype: PowerTime
+        """
+        reg = self.read_register(POWER_BUTTON_CONFIG)
         return PowerTime(get_bits(reg, 0, 2))
 
-    def set_power_button(self, enabled: bool):
-        reg = self.bus.read_i2c_block_data(self.address, POWER_BUTTON_CONFIG, 1)[0]
+    def set_power_button(self, enabled):
+        """
+        Enable or disable the power button function.
+        See page 16 for more information on the power button function.
+
+        :param enabled:
+        :type enabled: bool
+        :return:
+        """
+        reg = self.read_register(POWER_BUTTON_CONFIG)
         reg = set_bits(reg, enabled, 2)
-        self.bus.write_i2c_block_data(self.address, POWER_BUTTON_CONFIG, [reg])
+        self.write_register(POWER_BUTTON_CONFIG, reg)
         return
 
-    def get_power_button_setting(self) -> bool:
-        reg = self.bus.read_i2c_block_data(self.address, POWER_BUTTON_CONFIG, 1)[0]
+    def get_power_button_setting(self):
+        """
+        Get the status of the power button function.
+        See page 16 for more information on the power button function.
+
+        :return: Enabled status of power button
+        :rtype: bool
+        """
+        reg = self.read_register(POWER_BUTTON_CONFIG)
         res = get_bits(reg, 2)
         return res
 
     def is_power_button_touched(self):
-        # Check Power Button touch bit and clear interrupt
-        reg = self.bus.read_i2c_block_data(self.address, GENERAL_STATUS, 1)[0]
+        """
+        Check Power Button touch bit and clear interrupt.
+        See page 16 for more information on the power button function.
+
+        :return: Power button touch state
+        :rtype: bool
+        """
+        #
+        reg = self.read_register(GENERAL_STATUS)
         res = get_bits(reg, 4)
         if res:
             self.clear_interrupt()
             return True
         return False
+
+    def read_register(self, register):
+        """
+        Read value from register on CAP1203
+
+        :param register: Register address to acquire
+        :type register: int
+        :return: int (byte)
+        """
+        return self.bus.read_i2c_block_data(self.address, register, 1)[0]
+
+    def write_register(self, register, value):
+        """
+        Write value to register on CAP1203
+
+        :param register: Address of register you are writing to
+        :type register: int
+        :param value: Value written to register
+        :type value: int (0-255)
+        :return:
+        """
+        self.bus.write_i2c_block_data(self.address, register, [value])
+        return
